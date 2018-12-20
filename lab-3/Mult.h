a4d93b38528eb9c8dc1bc3ee6c5129e1bbfeaf51
Christopher Gentibano <cgent003@ucr.edu>
2018-12-19 17:15:44 -0800

    deleted duplicate

--- lab-3/Mult.h
+++
@@ -1,9 +0,0 @@
-#include "Oper.h"
-
-class Mult : public Oper {
-public:
-    Mult() : Oper(){};
-    Mult(Base* num1, Base* num2) : Oper(num1, num2) {};
-    
-    double evaluate() { return this->left->evaluate() * this->right->evaluate();};
-};
